1574c625e2731b47656d3932c4e7750d18f303dc
Ian McGillivary <wiian16@icloud.com>
2024-11-16 12:44:04 -0500

    Actually adding types file

---
+++ backend/db-interface.d.ts
@@ -0,0 +1,33 @@
+export interface Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    locationId ?: number;
+    title ?: string;
+}
+
+export interface Location {
+    id: number;
+    streetAddress: string;
+    secondaryAddress ?: string;
+    city: string;
+    region: string;
+    zipCode ?: string;
+    country: string;
+}
+
+export interface Product {
+    id: number;
+    name: string;
+    description ?: string;
+    contactPersonId: number;
+}
+
+export interface Repository {
+    id: number;
+    name: string;
+    description ?: string;
+    contactPersonId: number;
+}
