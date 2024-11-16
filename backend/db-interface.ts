7a092f1ea068eb86682ea9f13cf258681b43db79
Ian McGillivary <wiian16@icloud.com>
2024-11-16 12:39:48 -0500

    Added get all functions and type interfaced to db-interface

--- backend/db-interface.ts
+++ backend/db-interface.ts
@@ -1,5 +1,9 @@
-import pg from 'pg';
+import pg, {QueryResult} from 'pg';
 import config from './resources/pg-config.json'
+import Person from './db-interface'
+import Location from './db-interface'
+import Product from './db-interface'
+import Repository from './db-interface'
 
 export default class DBInterface {
     private client: pg.Client;
@@ -19,5 +23,30 @@ export default class DBInterface {
                 console.log("Database Connected");
             }
         });
+    }
+
+    public async getAllPeople(): Promise<Array<Person>> {
+        const res: QueryResult = await this.client.query("SELECT person_id, person_first_name, person_last_name, person_email, " +
+            "person_username, location_id, person_title FROM person;")
+
+        return (res.rows as Array<Person>);
+    }
+
+    public async getAllLocations(): Promise<Array<Location>> {
+        const res: QueryResult = await this.client.query("SELECT * FROM location");
+
+        return (res.rows as Array<Location>);
+    }
+
+    public async getAllProducts(): Promise<Array<Product>> {
+        const res: QueryResult = await this.client.query("SELECT * FROM product");
+
+        return (res.rows as Array<Product>);
+    }
+
+    public async getAllRepositories(): Promise<Array<Repository>> {
+        const res: QueryResult = await this.client.query("SELECT * FROM repository");
+
+        return (res.rows as Array<Repository>);
     }
 }
