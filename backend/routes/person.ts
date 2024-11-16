679386347f7a9b59acceea6961e7539e23847e85
matthewivan <73381386+matthewivan@users.noreply.github.com>
2024-11-16 16:11:21 -0500

    Add other methods to person endpoint

--- backend/routes/person.ts
+++ backend/routes/person.ts
@@ -13,6 +13,10 @@ router.use(cors());
 const _db = new DBInterface();
 
 router.get("/", (req, res) => getAllPeople(req, res));
+router.get("/first-name/:firstName", (req, res) => getPeopleByFirstName(req, res));
+router.get("/last-name/:lastName", (req, res) => getPeopleByLastName(req, res));
+router.get("/email/:email", (req, res) => getPeopleByEmail(req, res));
+router.get("/username/:username", (req, res) => getPeopleByUsername(req, res));
 router.get("/:id", (req, res) => getPeopleById(req, res));
 
 async function getAllPeople(_req: Request, res: Response) {
@@ -27,7 +31,59 @@ async function getPeopleById(_req: Request, res: Response){
     const _rows: Array<Person> = await _db.getPeopleById(_id);
 
     if(_rows.length == 0){
-        res.status(404).send(`No such person with id ${_id}`)
+        res.status(404).send(`No such person with id: ${_id}`)
+        return;
+    }
+
+    res.send(_rows[0]);
+}
+
+async function getPeopleByFirstName(_req: Request, res: Response){
+    const _firstName: string = _req.params.firstName as any as string;
+
+    const _rows: Array<Person> = await _db.getPeopleByFirstName(_firstName);
+
+    if(_rows.length == 0){
+        res.status(404).send(`No such person with first name: ${_firstName}`)
+        return;
+    }
+
+    res.send(_rows);
+}
+
+async function getPeopleByLastName(_req: Request, res: Response){
+    const _lastName: string = _req.params.lastName as any as string;
+
+    const _rows: Array<Person> = await _db.getPeopleByLastName(_lastName);
+
+    if(_rows.length == 0){
+        res.status(404).send(`No such person with last name: ${_lastName}`)
+        return;
+    }
+
+    res.send(_rows);
+}
+
+async function getPeopleByEmail(_req: Request, res: Response){
+    const _email: string = _req.params.email as any as string;
+
+    const _rows: Array<Person> = await _db.getPeopleByEmail(_email);
+
+    if(_rows.length == 0){
+        res.status(404).send(`No such person with email: ${_email}`)
+        return;
+    }
+
+    res.send(_rows[0]);
+}
+
+async function getPeopleByUsername(_req: Request, res: Response){
+    const _username: string = _req.params.username as any as string;
+
+    const _rows: Array<Person> = await _db.getPeopleByUsername(_username);
+
+    if(_rows.length == 0){
+        res.status(404).send(`No such person with username: ${_username}`)
         return;
     }
 
