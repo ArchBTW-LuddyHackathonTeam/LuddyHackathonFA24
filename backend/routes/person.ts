26120b26cebc81e80f909c30f6a69e4fa30f7c10
Ian McGillivary <wiian16@icloud.com>
2024-11-16 14:34:36 -0500

    Added get person by id endpoint

--- backend/routes/person.ts
+++ backend/routes/person.ts
@@ -6,7 +6,6 @@ import Person from '../db-interface'
 // import Product from '../db-interface'
 // import Repository from '../db-interface'
 import { Request, Response } from "express-serve-static-core";
-import { ParsedQs } from "qs";
 
 const router = express();
 
@@ -17,11 +16,25 @@ router.use(cors());
 const _db = new DBInterface();
 
 router.get("/", (req, res) => getAllPeople(req, res));
+router.get("/:id", (req, res) => getPeopleById(req, res));
 
-async function getAllPeople(_req: Request<{}, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>){
+async function getAllPeople(_req: Request, res: Response) {
     const _rows: Array<Person> = await _db.getAllPeople()
 
     res.send(_rows);
+}
+
+async function getPeopleById(_req: Request, res: Response){
+    const _id: number = _req.params.id as any as number;
+
+    const _rows: Array<Person> = await _db.getPeopleById(_id);
+
+    if(_rows.length == 0){
+        res.status(404).send(`No such person with id ${_id}}`)
+        return;
+    }
+
+    res.send(_rows[0]);
 }
 
 export default router;
