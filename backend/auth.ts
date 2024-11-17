0a518cbc380cd1dea4fee0d15ab776f55406b0c4
lillie <onlinechronically@protonmail.com>
2024-11-17 01:25:05 -0500

    extract: JWT Secret to variable

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,10 +1,12 @@
 import { SignJWT, JWTPayload } from "jose"
 import { Person } from './db-types'
 
+const jwtSecret = new TextEncoder().encode(process.env.JWTSECRET)
+
 export function forgeJWT(person: Person): Promise<String> {
   return new SignJWT({id: person.id, firstName: person.firstName, lastName: person.lastName} as JWTPayload)
   .setProtectedHeader({ alg: "HS256" })
   .setIssuedAt()
   .setExpirationTime("7d")
-  .sign(new TextEncoder().encode(process.env.JWTSECRET))
+  .sign(jwtSecret)
 }
