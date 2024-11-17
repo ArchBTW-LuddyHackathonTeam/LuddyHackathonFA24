f4b9f12ca47d52ee1b5b2d9d95727b355bc91a81
lillie <onlinechronically@protonmail.com>
2024-11-17 00:19:53 -0500

    attempt-add: session creation via JWT Tokens

--- backend/routes/sessions.ts
+++ backend/routes/sessions.ts
@@ -1,15 +1,29 @@
-import { Router } from "express"
+import express from "express"
+import { forgeJWT } from "../auth"
+import { Person } from "../db-types"
+import sessionCreate from "../schemas/sessionCreate"
+import DBInterface from "../db-interface"
 
-const router = Router()
+const router = express()
 
-router.post("/", (_req, res) => {
-  // TODO: insert SQL query here
-  if (false /* Invalid Login */) {
-    res.status(401).json({
-      message: "Invalid Login"
-    })
-  }
-  res.json({})
+const _db = new DBInterface()
+
+
+router.post("/", (req, res) => {
+  sessionCreate.validateAsync(req.body)
+  .then(body => _db.getUserByEmailAndPasswordHash(body.email, body.password))
+  .then(dbResults => {
+    if (dbResults.length == 0) return Promise.reject({ message: "Invalid E-Mail or Password" })
+    else return dbResults[0]
+  })
+  .then(body => forgeJWT({ id: body.id, firstName: body.firstName, lastName: body.lastName } as Person))
+  .then(token => {
+    res.cookie("tk", token, { maxAge: 604800000, httpOnly: true })
+    res.status(200).json({ success: true })
+  })
+  .catch(message => {
+    res.status(401).json({ error: message.message })
+  })
 })
 
 export default router
